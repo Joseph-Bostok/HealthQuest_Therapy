0945219e61402ceb9dbba6196e8d719669e98d9a
lauren-simineau <lsimine1@charlotte.edu>
2026-03-25 13:07:43 -0400

    Initial Commit

---
+++ healthQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  healthQuest
+//
+//  Created by Lauren Simineau on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ healthQuest/healthQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthQuestApp.swift
+//  healthQuest
+//
+//  Created by Lauren Simineau on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct healthQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
